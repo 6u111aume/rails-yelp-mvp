025f30370a046fbb8961a92e3ce662bf52ec0ef7
Guillaume Peterschmitt <guillaume.peterschmitt@gmail.com>
2019-05-20 16:51:24 +0200

    Exercice 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name: 'Comedia del Arte',
+    address: '8 Rue des Batignolles 75017 Paris France',
+    category: 'italian'
+  },
+  {
+    name: 'Samouraï Tavern',
+    address: '34 Rue Titon 75011 Paris France',
+    category: 'japanese'
+  },
+  {
+    name: 'Baguette et Pinot',
+    address: '118 Rue Amelot 75011 Paris France',
+    category: 'french'
+  },
+  {
+    name: 'Les frites du plat pays',
+    address: '75 Rue Legendre 75017 Paris France',
+    category: 'belgian'
+  },
+  {
+    name: 'Le Soleil Levant',
+    address: '119, avenue de Villiers 75017 Paris France',
+    category: 'chinese'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
